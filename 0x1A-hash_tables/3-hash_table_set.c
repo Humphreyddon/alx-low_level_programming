b054e93127c805dff6a52db256225f5c194001cc
Humphreyddon <Humphreydwhiz@gmail.com>
2022-10-20 14:29:32 -0700

    Uploading a function that adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,50 +1,65 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - adds item in a hash table
- * @ht: the table
- * @key: key of the item
- * @value: value of the item
- * Return: 1 for success 0 for failure
+ * add_n_hash - adds a node at the beginning of a hash at a given index
+ *
+ * @head: head of the hash linked list
+ * @key: key of the hash
+ * @value: value to store
+ * Return: head of the hash
  */
-
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+hash_node_t *add_n_hash(hash_node_t **head, const char *key, const char *value)
 {
-	unsigned long int i;
-	hash_node_t *new, *tmp;
+	hash_node_t *tmp;
 
-	if (!key || !(*key))
-		return (0);
-	new = malloc(sizeof(*new));
-	if (!new)
-		return (0);
-	new->key = strdup((char *)key);
-	new->value = strdup(value);
-	new->next = NULL;
-	i = key_index((unsigned char *)key, ht->size);
-	if (!(ht->array[i]))
+	tmp = *head;
+
+	while (tmp != NULL)
 	{
-		ht->array[i] = new;
-	}
-	else
-	{
-		/* check for update */
-		tmp = ht->array[i];
-		while (tmp && strcmp(tmp->key, new->key) != 0)
-			tmp = tmp->next;
-		if (tmp) /* its update */
+		if (strcmp(key, tmp->key) == 0)
 		{
 			free(tmp->value);
-			tmp->value = new->value;
-			free(new->key);
-			free(new);
-			return (1);
+			tmp->value = strdup(value);
+			return (*head);
 		}
-		/* its a collision */
-		tmp = ht->array[i];
-		new->next = tmp;
-		ht->array[i] = new;
+		tmp = tmp->next;
 	}
+
+	tmp = malloc(sizeof(hash_node_t));
+
+	if (tmp == NULL)
+		return (NULL);
+
+	tmp->key = strdup(key);
+	tmp->value = strdup(value);
+	tmp->next = *head;
+	*head = tmp;
+
+	return (*head);
+}
+
+/**
+ * hash_table_set - adds a hash (key, value) to a given hash table
+ *
+ * @ht: pointer to the hash table
+ * @key: key of the hash
+ * @value: value to store
+ * Return: 1 if successes, 0 if fails
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int k_index;
+
+	if (ht == NULL)
+		return (0);
+
+	if (key == NULL || *key == '\0')
+		return (0);
+
+	k_index = key_index((unsigned char *)key, ht->size);
+
+	if (add_n_hash(&(ht->array[k_index]), key, value) == NULL)
+		return (0);
 
 	return (1);
 }
